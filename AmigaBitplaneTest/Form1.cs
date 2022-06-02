076f33e6c32136e6ec1b096e90a7df2df269d8d1
Yossi <yossi.shelly@gmail.com>
2022-06-02 17:11:00 +0300

    first commit

---
+++ AmigaBitplaneTest/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AmigaBitplane;
+
+namespace AmigaBitplaneTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Bitplane bitplane = new Bitplane();
+                bitplane.LoadImage(ofd.FileName);
+            }
+        }
+    }
+}
